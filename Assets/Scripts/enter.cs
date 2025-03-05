07581eecac2116555d766636c414521278edef77
ALAMAMEN1 <maryoltchan1@gmail.com>
2025-03-05 01:12:02 +0100

    ala

---
+++ Assets/Scripts/enter.cs
@@ -0,0 +1,64 @@
+using System.Threading.Tasks;
+using System;
+using System.Globalization;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enter : MonoBehaviour
+{
+    public GameObject text;
+    public GameObject isTrigger;
+    //public AudioSource audioSource; 
+    public bool isEnter;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isEnter = true; 
+        text.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnter)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("E key was pressed."); 
+                isTrigger.SetActive(false);
+                //audioSource.Play();
+                isEnter = false;
+
+                int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+                if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+                {
+                    SceneManager.LoadScene(nextSceneIndex);
+                }
+                else
+                {
+                    Debug.Log("No more scenes to load.");
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            text.SetActive(true);
+            isEnter = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            text.SetActive(false);
+            isEnter = false;
+        }
+    }
+}
---
+++ Assets/Scripts/lose.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks;
+using System;
+using System.Globalization;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lose : MonoBehaviour
+{
+    public GameObject isTrigger;
+    //public AudioSource audioSource; 
+    public bool isEnter;
+
+    // Start is called once before the first execution of Update after the MonoBehavior is created
+    void Start()
+    {
+       isEnter = false;
+       isTrigger.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnter)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("trigger");
+        if (other.gameObject.tag == "Player")
+        {
+            isEnter = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isEnter = false;
+            isTrigger.SetActive(false);
+        }
+    }
+
+
+}
