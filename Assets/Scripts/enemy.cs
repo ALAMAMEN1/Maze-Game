eaab7b704103d75af9dc2768a2641a2b617e8592
ALAMAMEN1 <maryoltchan1@gmail.com>
2025-03-20 19:53:46 +0100

    ala

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -1,19 +1,20 @@
 using UnityEngine;
-
+using System.Collections;
 public class Enemy : MonoBehaviour
 {
 
     public GameObject player;
 
     public float speed;
-    public float dis;
-    public float min;
-    private float distance; 
+    public float stopDistance;
+    public float minDistance;
+    private float distance;
+    private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehavior is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,9 +25,18 @@ public class Enemy : MonoBehaviour
 
         direction = direction.normalized;
 
-        if(distance > dis && distance < min) {
+        if(distance > stopDistance && distance < minDistance) {
             transform.position = Vector2.MoveTowards(this.transform.position , player.transform.position, speed * Time.deltaTime);
+            animator.SetBool("play" , false);
         }
-
+        if(distance< stopDistance) {
+            Delay();
+        }
     }
+    private IEnumerator Delay(){
+        animator.SetBool("play",true);
+        yield return new WaitForSeconds(0.15f);
+        animator.SetBool("play",false);
+    }
+
 }
