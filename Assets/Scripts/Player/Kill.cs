8d5d62e05e9c2a6cb660a7a9a5622c5f0a75c256
ALAMAMEN1 <maryoltchan1@gmail.com>
2025-03-30 00:06:26 +0100

    add enemy to Scene 3 and fix Kill.cs

--- Assets/Scripts/Player/Kill.cs
+++ Assets/Scripts/Player/Kill.cs
@@ -6,7 +6,6 @@ public class Kill : MonoBehaviour
 
     public GameObject enemy;
 
-    public GameObject trigger;
 
     bool isEnter;
 
@@ -27,14 +26,14 @@ public class Kill : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy")){
+        if (other.CompareTag("playerW")){
             isEnter = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy")){
+        if (other.CompareTag("playerW")){
             isEnter = false;
         }
     }
